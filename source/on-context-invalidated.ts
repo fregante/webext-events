312e3d85f7f36431f4fecf51b7988ad988f4c77e
Federico Brigante <me@fregante.com>
2023-05-06 02:49:01 +0800

    Add `onContextInvalidated` listener (#7)

---
+++ source/on-context-invalidated.ts
@@ -0,0 +1,40 @@
+class OnContextInvalidated {
+	#timer: NodeJS.Timer | undefined;
+	#controller = new AbortController();
+
+	get signal() {
+		if (this.#timer) {
+			return this.#controller.signal;
+		}
+
+		this.#timer = setInterval(() => {
+			if (wasContextInvalidated()) {
+				this.#controller.abort();
+				clearInterval(this.#timer);
+			}
+		}, 200);
+
+		return this.#controller.signal;
+	}
+
+	get promise() {
+		return new Promise<void>(resolve => {
+			this.addListener(resolve);
+		});
+	}
+
+	addListener(callback: VoidCallback) {
+		if (this.signal.aborted) {
+			setTimeout(callback, 0);
+			return;
+		}
+
+		this.signal.addEventListener('abort', callback);
+	}
+}
+
+export const onContextInvalidated = new OnContextInvalidated();
+export const wasContextInvalidated = () => !chrome.runtime?.id;
+
+// This is done to ensure each listener and abortsignal are independent in each test
+export const _testing = OnContextInvalidated;
