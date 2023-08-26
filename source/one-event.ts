3d858f0744bcfb1a31d2964448b3284d0932f20f
Federico Brigante <me@fregante.com>
2023-08-26 20:04:43 +0700

    Lint

--- source/one-event.ts
+++ source/one-event.ts
@@ -6,19 +6,20 @@ type RemovableEvent<T = (...args: unknown[]) => unknown> = {
 type EventParameters<Event extends RemovableEvent<(...args: any[]) => void>> = Parameters<Parameters<Event['addListener']>[0]>;
 
 export async function oneEvent<
-	Event extends RemovableEvent<(...args: any[]) => void>,
+	Event extends RemovableEvent<(...parameters: any[]) => void>,
 >(
 	event: Event,
 	filter?: (...parameters: EventParameters<Event>) => boolean,
 ): Promise<void> {
 	await new Promise<void>(resolve => {
-		const listener = (...parameters: EventParameters<Event>) => {
+		// TODO: VoidFunction should not be necessary, it's equivalent to using "any"
+		const listener: VoidFunction = (...parameters: EventParameters<Event>) => {
 			if (!filter || filter(...parameters)) {
 				resolve();
-				event.removeListener(listener as VoidCallback);
+				event.removeListener(listener);
 			}
 		};
 
-		event.addListener(listener as VoidCallback);
+		event.addListener(listener);
 	});
 }
